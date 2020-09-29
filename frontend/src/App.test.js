d46fb684925f824981d0b09ff60296298cd7c929
TeohWR <71804378+TeohWR@users.noreply.github.com>
2020-09-29 22:17:37 +0800

    Update text search
    Text have changed from default, needed to update

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders NUS MONEY APP User List link', () => {
   const { getByText } = render(<App />);
-
+  const linkElement = getByText(/NUS MONEY APP User List/i);
+  expect(linkElement).toBeInTheDocument();
 });
